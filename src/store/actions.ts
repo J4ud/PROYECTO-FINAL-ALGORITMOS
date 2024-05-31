a38743b85b0ac7bbd8df360f73115e74ec174580
lulitohhh <luisa.mariaprf@gmail.com>
2024-05-31 16:33:29 -0500

    comentarios

--- src/store/actions.ts
+++ src/store/actions.ts
@@ -1,8 +1,10 @@
+//Esta función crea una acción para cambiar la pantalla actual en la aplicación.
 export const ChangeScreen = (screen:string) => {
     return{
         action: 'ChangeScreen',
         payload: screen
 }}
+//Esta función crea una acción que indica que el proceso de registro (sign-up) se ha completado.
 export const signUpCompleted = (screen:any) => {
     return{
         action: 'signUpCompleted',
