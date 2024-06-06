b6c551edc3b1632fc07cba4459eafa726586dc25
lulitohhh <luisa.mariaprf@gmail.com>
2024-06-06 18:54:48 -0500

    se crea el archivo foros.css

---
+++ src/screens/foros/foros.ts
@@ -0,0 +1,4 @@
+import SearchBar from '../../components/searchBar/searchBar';
+import Navbar from '../../components/navbar/navbar';
+import { MenuButton } from '../../components/index';
+import {SidebarMenu} from '../../components/index';;
